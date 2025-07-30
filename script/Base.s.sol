829765ebc1c9014ef525a873b1ceea798dcdb50a
Thomas Smets <thomass-smets@hotmail.com>
2025-07-30 18:53:49 +0200

    ✨ update deploy scripts

---
+++ script/Base.s.sol
@@ -0,0 +1,18 @@
+/**
+ * Created by Pragma Labs
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+pragma solidity ^0.8.30;
+
+import { Arcadia } from "./utils/Constants.sol";
+import { RecoveryController } from "../src/RecoveryController.sol";
+import { RecoveryToken } from "../src/RecoveryToken.sol";
+import { SafeTransactionBuilder } from "./utils/SafeTransactionBuilder.sol";
+import { Test } from "../lib/forge-std/src/Test.sol";
+
+abstract contract Base_Script is Test, SafeTransactionBuilder {
+    uint256 internal deployer = vm.envUint("PRIVATE_KEY_DEPLOYER");
+
+    RecoveryController internal recoveryController = RecoveryController(Arcadia.RECOVERY_CONTROLLER);
+    RecoveryToken internal recoveryToken = RecoveryToken(Arcadia.RECOVERY_TOKEN);
+}
--- script/DeployRecoveryContracts.s.sol
+++ script/DeployRecoveryContracts.s.sol
@@ -4,62 +4,16 @@
  */
 pragma solidity 0.8.30;
 
+import { Assets, Deployers, Safes } from "./utils/Constants.sol";
+import { Base_Script } from "./Base.s.sol";
 import { RecoveryController } from "../src/RecoveryController.sol";
-import { Script } from "../lib/forge-std/src/Script.sol";
-import { stdJson } from "../lib/forge-std/src/StdJson.sol";
 
-contract DeployRecoveryContracts is Script {
-    using stdJson for string;
-
-    /*///////////////////////////////////////////////////////////////
-                            CONSTANTS
-    ///////////////////////////////////////////////////////////////*/
-
-    address internal constant USDC_ADDRESS = 0x7F5c764cBc14f9669B88837ca1490cCa17c31607;
-
-    /*///////////////////////////////////////////////////////////////
-                            VARIABLES
-    ///////////////////////////////////////////////////////////////*/
-
-    uint256 internal broadcasterPrivateKey;
-
-    /*///////////////////////////////////////////////////////////////
-                            CONTRACTS
-    ///////////////////////////////////////////////////////////////*/
-
-    RecoveryController internal recoveryController;
-
-    /*///////////////////////////////////////////////////////////////
-                            CONSTRUCTOR
-    ///////////////////////////////////////////////////////////////*/
-
-    constructor() {
-        broadcasterPrivateKey = vm.envUint("PRIVATE_KEY_DEPLOYER_OPTIMISM");
-    }
-
-    /*///////////////////////////////////////////////////////////////
-                        RUN DEPLOY SCRIPT
-    ///////////////////////////////////////////////////////////////*/
-
+contract DeployRecoveryContracts is Base_Script {
     function run() external {
+        require(vm.addr(deployer) == Deployers.ARCADIA, "Wrong deployer.");
+
         // Deploy Recovery Contracts.
-        vm.broadcast(broadcasterPrivateKey);
-        recoveryController = new RecoveryController(USDC_ADDRESS);
-
-        // Mint Recovery Tokens.
-        (uint256[] memory amounts, address[] memory tos) = getMintData();
-        vm.broadcast(broadcasterPrivateKey);
-        recoveryController.batchMint(tos, amounts);
-    }
-
-    function getMintData() internal view returns (uint256[] memory amounts, address[] memory tos) {
-        // Read Json.
-        string memory root = vm.projectRoot();
-        string memory path = string.concat(root, "/script/mint_data.json");
-        string memory json = vm.readFile(path);
-
-        // Parse Json.
-        amounts = json.readUintArray(".amounts");
-        tos = json.readAddressArray(".tos");
+        vm.broadcast(deployer);
+        recoveryController = new RecoveryController(Safes.OWNER, Assets.USDC);
     }
 }
