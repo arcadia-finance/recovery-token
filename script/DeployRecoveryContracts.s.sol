86ceb417b51ef7414a0ff93d0c9362d664068552
Thomas Smets <thomass-smets@hotmail.com>
2025-08-12 16:03:38 +0200

    🎨 order imports

--- script/DeployRecoveryContracts.s.sol
+++ script/DeployRecoveryContracts.s.sol
@@ -5,9 +5,9 @@
 pragma solidity 0.8.30;
 
 import { Assets, Deployers, FeeRedeemer, Safes } from "./utils/Constants.sol";
-import { Redeemer } from "../src/Redeemer.sol";
 import { Base_Script } from "./Base.s.sol";
 import { RecoveryController } from "../src/RecoveryController.sol";
+import { Redeemer } from "../src/Redeemer.sol";
 
 contract DeployRecoveryContracts is Base_Script {
     function run() external {
